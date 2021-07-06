19dcda474f4281c547de415e5d46c754e9f9fb74
Adam Holder <31784045+BeakerThe1st@users.noreply.github.com>
2021-07-07 01:24:21 +1000

    do a little removing :rewind:

--- src/lib/environment.ts
+++ src/lib/environment.ts
@@ -7,16 +7,14 @@ let CLIENT_SECRET: string;
 const initialiseEnvironment = () => {
     dotenv.config();
 
-    console.log('initialising');
-
     if (process.env.PUBLIC_KEY === undefined || process.env.CLIENT_ID === undefined || process.env.CLIENT_SECRET === undefined) {
         console.log('Missing env values! Program will now exit');
         process.exit(0);
     }
 
-    PUBLIC_KEY = process.env.PUBLIC_KEY
-    CLIENT_ID = process.env.CLIENT_ID
-    CLIENT_SECRET = process.env.CLIENT_SECRET
+    PUBLIC_KEY = process.env.PUBLIC_KEY;
+    CLIENT_ID = process.env.CLIENT_ID;
+    CLIENT_SECRET = process.env.CLIENT_SECRET;
 
 }
 
