f6ad5c49001a744ed38d8f0c16bad6a013bf92fe
Adam Holder <31784045+BeakerThe1st@users.noreply.github.com>
2021-07-07 02:28:07 +1000

    we do a little reducing :game_die:

--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,21 @@
 import express from 'express';
 
-import { InteractionResponseType, InteractionType, verifyKeyMiddleware } from 'discord-interactions';
+import { InteractionType, verifyKeyMiddleware } from 'discord-interactions';
 
-import { PUBLIC_KEY } from './lib/environment.js';
+import { PUBLIC_KEY } from './lib/authentication.js';
+
+import { handleCommand } from './commands.js';
 
 const app: express.Application = express();
 const port = process.env.PORT || 6969;
 
-
 app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), (req: express.Request, res: express.Response) => {
-
-    const interaction = req.body;
+    let interaction = req.body;
 
     if (interaction.type === InteractionType.APPLICATION_COMMAND) {
-        res.send({
-            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-            data: {
-                content: 'yo yo yo willow smells',
-            }
-        })
+        handleCommand()
     }
-})
-
-app.use(express.json());
-
-app.get('/', (req: express.Request, res: express.Response) => {
-    res.send("Hello World!").status(200);
-    console.log('we got a get');
-});
-
+}));
 
 const initialiseServer = () => {
     app.listen(port, () => {
