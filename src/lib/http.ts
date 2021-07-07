dc0a4e9c336f1d0c4de5dc4e87544bae10fbce40
Adam Holder <31784045+BeakerThe1st@users.noreply.github.com>
2021-07-08 06:42:28 +1000

    add cool functions that do stuff :mouse:

--- src/lib/http.ts
+++ src/lib/http.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import querystring from 'querystring';
 import { InteractionResponse, Token } from '../types';
 
-import { CLIENT_ID, CLIENT_SECRET } from './authentication.js';
+import { CLIENT_ID, CLIENT_SECRET, APP_ID, getBearerToken } from './authentication.js';
 
 const getTokenResponse = async (): Promise<Token> => {
     let response = await axios(`https://discord.com/api/v8/oauth2/token`, {
@@ -24,8 +24,63 @@ const getTokenResponse = async (): Promise<Token> => {
     return token;
 }
 
-const editInteractionResponse = async (token: string, response: InteractionResponse) => {
-
+const editInteractionResponse = async (token: string, interactionResponse: InteractionResponse) => {
+    await axios(`https://discord.com/api/v8/webooks/${APP_ID}/${token}/messages/@original`, {
+        method: 'PATCH',
+        data: interactionResponse
+    });
 }
 
-export { getTokenResponse }
+const createGlobalCommand = async (commandData: any) => {
+    let response = await axios(`https://discord.com/api/v8/applications/${APP_ID}/commands`, {
+        method: 'POST',
+        data: commandData,
+        headers: {
+            "Authorization": `Bearer ${await getBearerToken()}`
+        }
+    })
+}
+
+const tagManagers = ['663903105133576202', '411543613076406294'];
+
+const createGuildCommand = async (commandData: any, guildId: string, tagManagerOnly: boolean) => {
+    if (tagManagerOnly) {
+        commandData.default_permission = false;
+    }
+
+    let response = await axios(`https://discord.com/api/v8/applications/${APP_ID}/guilds/${guildId}/commands`, {
+        method: 'POST',
+        data: commandData,
+        headers: {
+            "Authorization": `Bearer ${await getBearerToken()}`
+        }
+    })
+
+    if (tagManagerOnly) {
+        let { commandId } = response.data;
+        let permissions: any[] = [];
+        tagManagers.forEach((roleId) => {
+            permissions.push({
+                'id': roleId,
+                'type': 1,
+                'permission': true
+            })
+        })
+
+        try {
+            await axios(`https://discord.com/api/v8/applications/${APP_ID}/guilds/${guildId}/commands/${commandId}/permissions`, {
+                method: 'PUT',
+                data: {
+                    "permissions": permissions
+                },
+                headers: {
+                    "Authorization": `Bearer ${await getBearerToken()}`
+                }
+            })
+        } catch (error) {
+            console.error(error);
+        }
+    }
+}
+
+export { getTokenResponse, editInteractionResponse, createGlobalCommand, createGuildCommand }
