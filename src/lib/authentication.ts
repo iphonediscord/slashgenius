56e67afed4a6d118e80397d4de37af3536c0e400
Adam Holder <31784045+BeakerThe1st@users.noreply.github.com>
2021-07-07 14:37:01 +1000

    change how we get our bearer token :swimmer:

--- src/lib/authentication.ts
+++ src/lib/authentication.ts
@@ -3,25 +3,43 @@ import dotenv from 'dotenv';
 import axios from 'axios';
 import querystring from 'querystring';
 
+<<<<<<< Updated upstream
 let PUBLIC_KEY: string = "808e1ba34b0ba4940e8cb60a1fc945433a2d2b1f38ba747e7827bb02c09a23fc";
 let CLIENT_ID: string;
 let CLIENT_SECRET: string;
+=======
+import { Token } from '../types';
+
+let PUBLIC_KEY: string = "808e1ba34b0ba4940e8cb60a1fc945433a2d2b1f38ba747e7827bb02c09a23fc";
+let CLIENT_ID: string;
+let CLIENT_SECRET: string;
+let DB_URI: string;
+>>>>>>> Stashed changes
 
 let initialised: boolean;
 
 const initialiseEnvironment = () => {
     dotenv.config();
 
+<<<<<<< Updated upstream
     if (process.env.CLIENT_ID === undefined || process.env.CLIENT_SECRET === undefined) {
+=======
+    if (process.env.CLIENT_ID === undefined || process.env.CLIENT_SECRET === undefined || process.env.DB_URI === undefined) {
+>>>>>>> Stashed changes
         console.log('Missing env values! Program will now exit');
         process.exit(0);
     }
     CLIENT_ID = process.env.CLIENT_ID;
     CLIENT_SECRET = process.env.CLIENT_SECRET;
+<<<<<<< Updated upstream
+=======
+    DB_URI = process.env.DB_URI;
+>>>>>>> Stashed changes
 
     initialised = true;
 }
 
+<<<<<<< Updated upstream
 interface Token {
     "access_token": string,
     "expires_at": number,
@@ -31,6 +49,11 @@ let currentToken: Token | null = null;
 
 const getInteractionToken = async (): Promise<String> => {
 
+=======
+let currentToken: Token | null = null;
+
+const getBearerToken = async (): Promise<String> => {
+>>>>>>> Stashed changes
     if (!initialised) {
         initialiseEnvironment();
     }
@@ -38,6 +61,7 @@ const getInteractionToken = async (): Promise<String> => {
     const secondsSinceEpoch = Math.round(Date.now() / 1000);
 
     if (currentToken === null || currentToken.expires_at < secondsSinceEpoch) {
+<<<<<<< Updated upstream
         //Current token does not exist or it has expired.
         //Now we need a new one
         try {
@@ -69,3 +93,13 @@ const getInteractionToken = async (): Promise<String> => {
 };
 
 export { initialiseEnvironment, getInteractionToken, PUBLIC_KEY, CLIENT_ID, CLIENT_SECRET }
+=======
+        currentToken = await getBearerToken();
+    }
+
+    return currentToken.access_token;
+
+};
+
+export { initialiseEnvironment, getBearerToken, PUBLIC_KEY, CLIENT_ID, CLIENT_SECRET, DB_URI }
+>>>>>>> Stashed changes
---
+++ src/lib/http.ts
@@ -0,0 +1,31 @@
+import axios from 'axios'
+import querystring from 'querystring';
+import { InteractionResponse, Token } from '../types';
+
+import { CLIENT_ID, CLIENT_SECRET } from './authentication.js';
+
+const getBearerToken = async (): Promise<Token> => {
+    let response = await axios(`https://discord.com/api/v8/oauth2/token`, {
+        method: 'post',
+        data: querystring.stringify({ 'grant_type': 'client_credentials', 'scope': 'applications.commands.update' }),
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        auth: {
+            username: CLIENT_ID,
+            password: CLIENT_SECRET
+        }
+    });
+
+    let token: Token = {
+        access_token: response.data.access_token,
+        expires_at: (response.data.expires_in + Math.round(Date.now() / 1000))
+    }
+    return token;
+}
+
+const editInteractionResponse = async (token: string, response: InteractionResponse) => {
+
+}
+
+export { getBearerToken }
