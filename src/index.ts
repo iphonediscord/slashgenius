11a8ce00b2c91bcb99900c75a3cce444471fa0da
Adam Holder <31784045+BeakerThe1st@users.noreply.github.com>
2021-07-08 06:42:05 +1000

    test command, you suck :wave:

--- src/index.ts
+++ src/index.ts
@@ -6,47 +6,6 @@ initialiseEnvironment();
 import { initialiseServer } from './server.js'
 initialiseServer();
 
+import { createDefaultCommands } from './commands/commands.js';
 
-const createTestCommand = async () => {
-    let url = `https://discord.com/api/v8/applications/${CLIENT_ID}/guilds/409759972986191872/commands`;
-
-    let command = {
-        "name": "test",
-        "description": "Tests the slashGenius bot",
-        "options": [
-            {
-                "name": "display_text",
-                "description": "Text to display back",
-                "type": 3,
-                "required": false
-            }
-        ]
-    }
-
-    let commandHeaders = {
-        "Authorization": `Bearer ${await getBearerToken()}`
-    };
-
-    try {
-        let response = await axios({
-            method: 'post',
-            url: url,
-            data: command,
-            headers: commandHeaders
-        })
-    } catch (error) {
-        if (axios.isAxiosError(error)) {
-            if (error.response) {
-                console.log(`${error.response.status} :`, error.response.data);
-            } else if (error.request) {
-                console.log(error.request);
-            } else {
-                console.log(`Axios error`, error.message);
-            }
-        } else {
-            console.error(`(unexpected) ${error}`);
-        }
-    }
-}
-
-createTestCommand();
+createDefaultCommands();
