ad14e24ac29bbaac9eebda1bcb47fde19dd4f776
Adam Holder <31784045+BeakerThe1st@users.noreply.github.com>
2021-07-08 06:42:18 +1000

    add app id to auth file :pensive:

--- src/lib/authentication.ts
+++ src/lib/authentication.ts
@@ -11,19 +11,21 @@ let PUBLIC_KEY: string = "808e1ba34b0ba4940e8cb60a1fc945433a2d2b1f38ba747e7827bb
 let CLIENT_ID: string;
 let CLIENT_SECRET: string;
 let DB_URI: string;
+let APP_ID: string;
 
 let initialised: boolean;
 
 const initialiseEnvironment = () => {
     dotenv.config();
 
-    if (process.env.CLIENT_ID === undefined || process.env.CLIENT_SECRET === undefined || process.env.DB_URI === undefined) {
+    if (process.env.CLIENT_ID === undefined || process.env.CLIENT_SECRET === undefined || process.env.DB_URI === undefined || process.env.APP_ID === undefined) {
         console.log('Missing env values! Program will now exit');
         process.exit(0);
     }
     CLIENT_ID = process.env.CLIENT_ID;
     CLIENT_SECRET = process.env.CLIENT_SECRET;
     DB_URI = process.env.DB_URI;
+    APP_ID = process.env.APP_ID;
 
     initialised = true;
 }
@@ -45,4 +47,4 @@ const getBearerToken = async (): Promise<string> => {
 
 };
 
-export { initialiseEnvironment, getBearerToken, PUBLIC_KEY, CLIENT_ID, CLIENT_SECRET, DB_URI }
+export { initialiseEnvironment, getBearerToken, PUBLIC_KEY, CLIENT_ID, CLIENT_SECRET, DB_URI, APP_ID }
