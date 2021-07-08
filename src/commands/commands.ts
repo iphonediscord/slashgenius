3e15266fb038412fef3009358da850a5b1e8fbe2
Adam Holder <31784045+BeakerThe1st@users.noreply.github.com>
2021-07-09 06:03:21 +1000

    and I oop tstststststst :laughing:

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -3,59 +3,9 @@ import { Request, Response } from 'express';
 import { InteractionResponseType, InteractionType } from 'discord-interactions';
 import { connect, disconnect } from '../database/database.js';
 import { Tag } from '../database/tags.js';
-import { createGuildCommand, editInteractionResponse } from '../lib/http.js';
 
 const createDefaultCommands = () => {
-    createGuildCommand({
-        "name": "create",
-        "description": "Creates a tag",
-        "options": [
-            {
-                "name": "name",
-                "description": "Name of the tag",
-                "type": 3,
-                "required": true
-            },
-            {
-                "name": "content",
-                "description": "Content of the tag",
-                "type": "3",
-                "required": true
-            }
-        ]
-    }, '409759972986191872', true);
 
-    createGuildCommand({
-        "name": "edit",
-        "description": "Edits a tag",
-        "options": [
-            {
-                "name": "name",
-                "description": "Name of the tag",
-                "type": 3,
-                "required": true
-            },
-            {
-                "name": "new-content",
-                "description": "New content of the tag",
-                "type": "3",
-                "required": true
-            }
-        ]
-    }, '409759972986191872', true);
-
-    createGuildCommand({
-        "name": "delete",
-        "description": "Deletes a tag",
-        "options": [
-            {
-                "name": "name",
-                "description": "Name of the tag",
-                "type": 3,
-                "required": true
-            }
-        ]
-    }, '409759972986191872', true);
 }
 
 const handleCommand = (interaction: any) => {
