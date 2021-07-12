5d41b586a932445577f47313677305e12798921b
Adam Holder <31784045+BeakerThe1st@users.noreply.github.com>
2021-07-12 19:51:24 +1000

    Commit da build 4 sum reason :volcano:

--- build/server.js
+++ build/server.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import { InteractionType, verifyKeyMiddleware } from 'discord-interactions';
-import { PUBLIC_KEY } from './lib/authentication.js';
 import { handleCommand } from './commands/commands.js';
 const app = express();
 const port = process.env.PORT || 6969;
-app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), (req, res) => {
+if (process.env.PUBLIC_KEY === undefined) {
+    console.log('Missing PUBLIC_KEY environment variable. Process will now exit.');
+    process.exit(0);
+}
+app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), (req, res) => {
     let interaction = req.body;
     if (interaction.type === InteractionType.APPLICATION_COMMAND) {
         let response = {
