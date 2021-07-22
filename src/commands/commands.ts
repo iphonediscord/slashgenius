516dccf26fa9bc7d546fd5dc6f2a0b593c2b56d3
Adam Holder <31784045+BeakerThe1st@users.noreply.github.com>
2021-07-22 22:57:23 +1000

    Change command creation and handling

--- src/commands/commands.ts
+++ src/commands/commands.ts
@@ -1,14 +1,33 @@
 import { connect, disconnect } from '../database/database.js';
 import { ResponseAPI } from '../discord/ReponseAPI.js';
-import { Interaction, WebhookMessageContent } from '../types/discord.types.js';
+import { CommandParams, CommandPermissions, GuildCommandPermissions, Interaction, WebhookMessageContent } from '../types/discord.types.js';
 import { GuildCommandAPI } from '../discord/CommandAPI.js';
 import defaults from './defaults.json';
+import mongoose from 'mongoose';
+import { Tag } from '../database/tags.js';
+import { TagDocument } from '../types/database.types.js';
+
+let managerRoles = ['411543613076406294', '663903105133576202'];
+let rIphoneId = '409759972986191872'
 
 const registerDefaultCommands = () => {
-    let api = new GuildCommandAPI('409759972986191872');
-    defaults.forEach((params) => {
+    let api = new GuildCommandAPI(rIphoneId);
+    defaults.forEach(async (params) => {
         try {
-            api.createCommand(params);
+            let command = await api.createCommand(params);
+            if (params.managerOnly) {
+                let permissions: CommandPermissions[] = [];
+                
+                managerRoles.forEach((roleId) => {
+                    permissions.push({
+                        'id': roleId,
+                        'type': 1,
+                        permission: true
+                    })
+                });
+
+                api.editPermissions(command.id, permissions)
+            }
         } catch (error) {
             console.log(`Error creating '${params.name}' command`, error);
         }
@@ -37,6 +56,9 @@ const handleCommand = (interaction: Interaction) => {
         case 'floppa':
             showFloppa(interaction);
             break;
+        case 'update':
+            updateCommands(interaction);
+            break;
         default:
             displayTag(interaction);
             break;
@@ -46,7 +68,7 @@ const handleCommand = (interaction: Interaction) => {
 
 }
 
-const createTag = (interaction: Interaction) => {
+const createTag = async (interaction: Interaction) => {
     let newContent: WebhookMessageContent = {
         content: 'Successfully created sussy baka!'
     }
@@ -54,7 +76,7 @@ const createTag = (interaction: Interaction) => {
     new ResponseAPI(interaction.token).editOriginalResponse(newContent);
 }
 
-const showFloppa = (interaction: Interaction) => {
+const showFloppa = async (interaction: Interaction) => {
     let newContent: WebhookMessageContent = {
         content: 'https://tenor.com/view/floppa-gif-22061121'
     }
@@ -62,19 +84,41 @@ const showFloppa = (interaction: Interaction) => {
     new ResponseAPI(interaction.token).editOriginalResponse(newContent);
 }
 
-const editTag = (interaction: Interaction) => {
+const editTag = async (interaction: Interaction) => {
 
 }
 
-const deleteTag = (interaction: Interaction) => {
+const deleteTag = async (interaction: Interaction) => {
 
 }
 
-const displayTag = (interaction: Interaction) => {
+const updateCommands = async (interaction: Interaction) => {
+    const allTags = await Tag.find();
+
+    let commands: CommandParams[] = [];
+
+    allTags.forEach((tag: TagDocument) => {
+        commands.push({
+            'name': tag.name,
+            'description': `Displays the '${tag.name}' tag.`
+        });
+    });
+
+    new GuildCommandAPI(rIphoneId).bulkOverwriteCommands(commands)
+}
+
+const displayTag = async (interaction: Interaction) => {
     let { name } = interaction.data;
 
     //Lookup name in db and display it
-    
+    Tag.findOne({ 'name': name }, 'name content', {}, (err, tag) => {
+        let api = new ResponseAPI(interaction.token);
+        if (err || tag === null || tag.content === null) {
+            api.editOriginalResponse({ content: `There was an error displaying the \`${name}\` tag. Please notify server staff.` });
+        } else {
+            api.editOriginalResponse({ content: tag.content });
+        }
+    })
 }
 
 export { handleCommand, registerDefaultCommands }
